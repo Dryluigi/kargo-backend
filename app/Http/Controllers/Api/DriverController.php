334bdebf768e33ee71c975177a890cc5f7fd4fe6
sinulingga23 <sinulinggatwo@gmail.com>
2022-08-13 12:52:26 +0700

    feat: fix upload image

--- app/Http/Controllers/Api/DriverController.php
+++ app/Http/Controllers/Api/DriverController.php
@@ -20,14 +20,20 @@ class DriverController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
+        
+        $upload_folder = 'drivers';
+        $image_id_card_file_name = $request->file('id_card_file_name');
+        $image_driver_license_file_name = $request->file('driver_license_file_name');
 
-        // TODO: Simpan file yang dikirim ke storage
+        $image_uploaded_path_id_card_file_name = $image_id_card_file_name->store($upload_folder, 'public');
+        $image_uploaded_path_driver_license_file_name = $image_driver_license_file_name->store($upload_folder, 'public');
+
         try {
             $driver = Driver::create([
                 'name' => $request->name,
                 'phone_number' => $request->phone_number,
-                'id_card_file_name' => $request->id_card_file_name,
-                'driver_license_file_name' => $request->driver_license_file_name,
+                'id_card_file_name' => $image_uploaded_path_id_card_file_name,
+                'driver_license_file_name' => $image_uploaded_path_driver_license_file_name,
                 'is_active' => true,
             ]);
 
